19d5288a9dfd20abc8c1cdcaca432f01fbafbb20
Aka Y <aliffathoni7@gmail.com>
2022-11-15 20:59:12 +0700

    fix details

--- src/config.cpp
+++ src/config.cpp
@@ -10,6 +10,9 @@ SiSCA::SiSCA(String bluetoothName, uint8_t ledPin, uint8_t buzzerPin, uint8_t ba
   _adcPin = adcPin;
 }
 
+/*
+ *  @brief start all necessary sensors and output
+*/
 void SiSCA::begin(void)
 {
   ledBegin(_ledPin);
@@ -19,6 +22,9 @@ void SiSCA::begin(void)
   mpuBegin();
 }
 
+/*
+ *  @brief start SiSCA system
+*/
 void SiSCA::startSystem(void)
 {
   ledOn("Green");
@@ -27,36 +33,65 @@ void SiSCA::startSystem(void)
   delay(500);
 }
 
+/*!
+ *    @brief  Get heart rate count data from sensor.
+ *
+ *    @return Integer value of Heart Rate measurement.
+ */
 int SiSCA::getHR(void)
 {
   int hr = getDataHR();
   return hr;
 }
 
+/*!
+ *    @brief  Get ECG data from sensor.
+ *
+ *    @return Integer value of ECG analog input value.
+ */
 int SiSCA::getECG(void)
 {
   int ecg = getDataECG();
   return ecg;
 }
 
+/*!
+ *    @brief  Get quick diagnostic data based on ECG data.
+ *
+ *    @return String of quick diagnose result.
+ */
 String SiSCA::getDiagnose(void)
 {
   String diagnose = quickDetect();
   return diagnose;
 }
 
+/*!
+ *    @brief  Get device battery percentage.
+ *
+ *    @return Integer value of battery percentage.
+ */
 int SiSCA::getBattery(void)
 {
   int battery = getDataBattery();
   return battery;
 }
 
+/*!
+ *    @brief  Get current user activity.
+ *
+ *    @return String of current activity.
+ */
 String SiSCA::getActivity(void)
 {
   String activity = getPosition();
   return activity;
 }
 
+/*!
+ *    @brief  Send Heart Rate, Quick Diagnostic, Battery percentage
+ *            and User Activity data to client.
+ */
 void SiSCA::sendData(void)
 {
   send_HR_Data(getDataHR);
@@ -65,6 +100,9 @@ void SiSCA::sendData(void)
   send_User_Activity(getActivity);
 }
 
+/*!
+ *    @brief  Send specific ECG data to client.
+ */
 void SiSCA::sendECGData(void)
 {
   send_ECG_Data(getECG);
